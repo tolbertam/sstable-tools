3b4b5c87247a6747529272b10caf6a6f297a9303
Chris Lohfink <Chris.Lohfink@datastax.com>
2015-12-12 00:53:16 -0600

    basic reader

--- src/main/java/com/csforge/reader/CassandraReader.java
+++ src/main/java/com/csforge/reader/CassandraReader.java
@@ -3,16 +3,14 @@ package com.csforge.reader;
 import org.apache.cassandra.config.CFMetaData;
 import org.apache.cassandra.config.Schema;
 import org.apache.cassandra.cql3.QueryProcessor;
+import org.apache.cassandra.cql3.statements.CFStatement;
 import org.apache.cassandra.cql3.statements.CreateTableStatement;
+import org.apache.cassandra.cql3.statements.ParsedStatement;
 import org.apache.cassandra.db.DecoratedKey;
-import org.apache.cassandra.db.rows.Row;
-import org.apache.cassandra.db.rows.Unfiltered;
 import org.apache.cassandra.db.rows.UnfilteredRowIterator;
-import org.apache.cassandra.exceptions.RequestValidationException;
-import org.apache.cassandra.io.sstable.Component;
 import org.apache.cassandra.io.sstable.Descriptor;
 import org.apache.cassandra.io.sstable.format.SSTableReader;
-import org.apache.cassandra.tools.Util;
+import org.apache.cassandra.schema.*;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -27,20 +25,24 @@ public class CassandraReader {
     CFMetaData metadata = null;
 
     public CassandraReader(String cql) {
-        metadata = ((CreateTableStatement) QueryProcessor.parseStatement(cql).prepare().statement).getCFMetaData();
+        Schema.instance.setKeyspaceMetadata(KeyspaceMetadata.create("turtle", KeyspaceParams.local(), Tables.none(), Views.none(), Types.none(), Functions.none()));
+        CFStatement statement = (CFStatement) QueryProcessor.parseStatement(cql);
+        statement.prepareKeyspace("turtle");
+        metadata = ((CreateTableStatement) statement.prepare().statement).getCFMetaData();
     }
 
     public Stream<DecoratedKey> keys(Stream<String> keys) {
         return null;
     }
 
-    public Stream<Partition> readSSTable(String sstablePath, Set excludes) throws IOException {
+    public Stream<Partition> readSSTable(String sstablePath, Set<DecoratedKey> excludes) throws IOException {
         Descriptor desc = Descriptor.fromFilename(sstablePath);
         SSTableReader sstable = SSTableReader.openNoValidation(desc, metadata);
 
         Spliterator<UnfilteredRowIterator> spliterator = Spliterators.spliteratorUnknownSize(sstable.getScanner(), Spliterator.IMMUTABLE);
         Stream<UnfilteredRowIterator> stream = StreamSupport.stream(spliterator, false);
-        return stream.map(Partition::new);
+        return stream.filter(i -> excludes != null && excludes.contains(i.partitionKey()))
+                     .map(Partition::new);
     }
 
 }
