8b5b089f5b4ee5c5062595e1563db3f37981f827
Chris Lohfink <Chris.Lohfink@datastax.com>
2015-12-12 00:04:46 -0600

    sstable wrapper

--- src/main/java/com/csforge/reader/CassandraReader.java
+++ src/main/java/com/csforge/reader/CassandraReader.java
@@ -5,6 +5,9 @@ import org.apache.cassandra.config.Schema;
 import org.apache.cassandra.cql3.QueryProcessor;
 import org.apache.cassandra.cql3.statements.CreateTableStatement;
 import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.rows.Row;
+import org.apache.cassandra.db.rows.Unfiltered;
+import org.apache.cassandra.db.rows.UnfilteredRowIterator;
 import org.apache.cassandra.exceptions.RequestValidationException;
 import org.apache.cassandra.io.sstable.Component;
 import org.apache.cassandra.io.sstable.Descriptor;
@@ -14,7 +17,10 @@ import org.apache.cassandra.tools.Util;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class CassandraReader {
 
@@ -32,7 +38,9 @@ public class CassandraReader {
         Descriptor desc = Descriptor.fromFilename(sstablePath);
         SSTableReader sstable = SSTableReader.openNoValidation(desc, metadata);
 
-        return null;
+        Spliterator<UnfilteredRowIterator> spliterator = Spliterators.spliteratorUnknownSize(sstable.getScanner(), Spliterator.IMMUTABLE);
+        Stream<UnfilteredRowIterator> stream = StreamSupport.stream(spliterator, false);
+        return stream.map(Partition::new);
     }
 
 }
--- src/main/java/com/csforge/reader/Partition.java
+++ src/main/java/com/csforge/reader/Partition.java
@@ -1,28 +1,62 @@
 package com.csforge.reader;
 
+import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.PartitionColumns;
 import org.apache.cassandra.db.rows.Row;
 import org.apache.cassandra.db.rows.Unfiltered;
+import org.apache.cassandra.db.rows.UnfilteredRowIterator;
 
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
+ * Layer that wraps UnfilteredRowIterator
  */
-public interface Partition {
+public class Partition {
+
+    UnfilteredRowIterator parent;
+
+    public Partition(UnfilteredRowIterator parent) {
+        this.parent = parent;
+    }
 
     /**
      * A timestamp (typically in microseconds since the unix epoch, although this is not enforced) after which
      * data should be considered deleted. If set to Long.MIN_VALUE, this implies that the data has not been marked
      * for deletion at all.
      */
-    public long markedForDeleteAt();
+    public long markedForDeleteAt() {
+        return parent.partitionLevelDeletion().markedForDeleteAt();
+    }
+
+    public boolean isLive() {
+        return markedForDeleteAt() > Long.MIN_VALUE;
+    }
 
     /**
      * The local server timestamp, in seconds since the unix epoch, at which this tombstone was created. This is
      * only used for purposes of purging the tombstone after gc_grace_seconds have elapsed.
      */
-    public int localDeletionTime();
+    public int localDeletionTime() {
+        return parent.partitionLevelDeletion().localDeletionTime();
+    }
 
-    public Row staticRow();
+    public DecoratedKey getKey() {
+        return parent.partitionKey();
+    }
 
-    public Stream<Unfiltered> rows();
+    public PartitionColumns columns() {
+        return parent.columns();
+    }
+
+    public Row staticRow() {
+        return parent.staticRow();
+    }
+
+    public Stream<Unfiltered> rows() {
+        Spliterator<Unfiltered> spliterator = Spliterators.spliteratorUnknownSize(parent, Spliterator.IMMUTABLE);
+        return StreamSupport.stream(spliterator, false);
+    }
 }
