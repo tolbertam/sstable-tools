8862420eb4d37e885f6ee27f41ad4ca11f0bd164
Chris Lohfink <Chris.Lohfink@datastax.com>
2016-03-17 11:30:38 -0500

    remove integration

--- src/integration/python/tests.py
+++
@@ -1,7 +0,0 @@
-import os
-import unittest
-
-if __name__ == "__main__":
-    print "Integration test suite for Cassandra %s" % os.environ['CASSANDRA_VERSION']
-    suite = unittest.TestSuite()
-    unittest.TextTestRunner(verbosity=2).run(suite)
